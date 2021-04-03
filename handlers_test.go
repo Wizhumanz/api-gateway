65ed75c4fcf1e30a871d8484c639be24f4f94007
Simon Jeong <simon20000808@gmail.com>
2021-04-03 14:03:31 +0900

    added test for getAllExchangeConnections

--- handlers_test.go
+++ handlers_test.go
@@ -113,7 +113,7 @@ func TestHandlerGetAllTrades(t *testing.T) {
 	dec := json.NewDecoder(strings.NewReader(newJsonStr))
 	err := dec.Decode(&listOfTrades)
 	if err != nil {
-		t.Error("Expected response body to be of type []Bot")
+		t.Error("Expected response body to be of type []TradeAction")
 	}
 	// for i, bot := range listOfBots {
 	// 	fmt.Println(i, bot.K.ID)
@@ -121,31 +121,84 @@ func TestHandlerGetAllTrades(t *testing.T) {
 	if len(listOfTrades) > 0 {
 		for _, trade := range listOfTrades {
 			if trade.Action == "" {
-				t.Error("Expected handler to return Bot structs with Action")
+				t.Error("Expected handler to return TradeAction structs with Action")
 			}
 			if trade.AggregateID == 0 {
-				t.Error("Expected handler to return Bot structs with AggregateID")
+				t.Error("Expected handler to return TradeAction structs with AggregateID")
 			}
 			if trade.BotID == "" {
-				t.Error("Expected handler to return Bot structs with BotID")
+				t.Error("Expected handler to return TradeAction structs with BotID")
 			}
 			if trade.OrderType != 0 && trade.OrderType != 1 {
-				t.Error("Expected handler to return Bot structs with OrderType")
+				t.Error("Expected handler to return TradeAction structs with OrderType")
 			}
 			if trade.Size == 0 {
-				t.Error("Expected handler to return Bot structs with Size")
+				t.Error("Expected handler to return TradeAction structs with Size")
 			}
 			if trade.TimeStamp == "" {
-				t.Error("Expected handler to return Bot structs with TimeStamp")
+				t.Error("Expected handler to return TradeAction structs with TimeStamp")
 			}
 			if trade.Ticker == "" {
-				t.Error("Expected handler to return Bot structs with Ticker")
+				t.Error("Expected handler to return TradeAction structs with Ticker")
 			}
 			if trade.Exchange == "" {
-				t.Error("Expected handler to return Bot structs with Exchange")
+				t.Error("Expected handler to return TradeAction structs with Exchange")
 			}
 			if trade.KEY == "" {
-				t.Error("Expected handler to return Bot structs with KEY")
+				t.Error("Expected handler to return TradeAction structs with KEY")
+			}
+		}
+	}
+
+	// fmt.Println(resp.Header.Get("Content-Type")
+
+}
+
+func TestHandlerGetAllExchangeConnections(t *testing.T) {
+	req := httptest.NewRequest("GET", "/trades?user="+"5632499082330112", nil)
+	req.Header.Set("Authorization", "trader")
+	w := httptest.NewRecorder()
+	getAllExchangeConnectionsHandler(w, req)
+
+	resp := w.Result()
+
+	if resp.StatusCode != 200 {
+		t.Error("Expected status code to equal 200")
+	}
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	newJsonStr := buf.String()
+	// fmt.Println(newJsonStr)
+
+	var listOfExchanges []ExchangeConnection
+	dec := json.NewDecoder(strings.NewReader(newJsonStr))
+	err := dec.Decode(&listOfExchanges)
+	if err != nil {
+		t.Error("Expected response body to be of type []ExchangeConnection")
+	}
+	// for i, bot := range listOfBots {
+	// 	fmt.Println(i, bot.K.ID)
+	// }
+	if len(listOfExchanges) > 0 {
+		for _, exchange := range listOfExchanges {
+			if exchange.Name == "" {
+				t.Error("Expected handler to return ExchangeConnection structs with Name")
+			}
+			if exchange.APIKey == "" {
+				t.Error("Expected handler to return ExchangeConnection structs with APIKey")
+			}
+			if exchange.UserID == "" {
+				t.Error("Expected handler to return ExchangeConnection structs with UserID")
+			}
+			if exchange.Timestamp == "" {
+				t.Error("Expected handler to return ExchangeConnection structs with Timestamp")
+			}
+			if exchange.KEY == "" {
+				t.Error("Expected handler to return ExchangeConnection structs with KEY")
+			}
+			if exchange.K.ID == 0 {
+				t.Error("Expected handler to return ExchangeConnection structs with DB key")
 			}
 		}
 	}
