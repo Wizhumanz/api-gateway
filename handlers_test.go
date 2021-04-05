e402f8c45288e0283292ea53b97633d4644b1ea2
Simon Jeong <simon20000808@gmail.com>
2021-04-06 01:12:56 +0900

    test for createNewUser

--- handlers_test.go
+++ handlers_test.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http/httptest"
 	"strings"
 	"testing"
+
+	"cloud.google.com/go/datastore"
+	"google.golang.org/api/iterator"
 )
 
 // func TestHandlerCreateNewBot(t *testing.T) {
@@ -205,4 +211,47 @@ func TestHandlerGetAllExchangeConnections(t *testing.T) {
 
 	// fmt.Println(resp.Header.Get("Content-Type")
 
+}
+func TestHandlerCreateNewUser(t *testing.T) {
+	values := map[string]string{"name": "JOHN DOE", "email": "VEGGIE@VEGGIE.COM", "password": "supersoaker"}
+	json_data, err := json.Marshal(values)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req := httptest.NewRequest("POST", "/user", bytes.NewBuffer(json_data))
+	w := httptest.NewRecorder()
+	createNewUserHandler(w, req)
+
+	resp := w.Result()
+
+	if resp.StatusCode != 201 {
+		t.Error("Expected status code to equal 201")
+	} else {
+		ctx := context.Background()
+		client, err := datastore.NewClient(ctx, googleProjectID)
+		if err != nil {
+			// TODO: Handle error.
+			log.Fatal(err)
+		}
+
+		query := datastore.NewQuery("User").Filter("Name =", "JOHN DOE")
+
+		//run query
+		t := client.Run(ctx, query)
+		var x User
+		for {
+			_, err := t.Next(&x)
+			if err == iterator.Done {
+				break
+			}
+		}
+		fmt.Println("working")
+		key := datastore.IDKey("User", x.K.ID, nil)
+		if err := client.Delete(ctx, key); err != nil {
+			// TODO: Handle error.
+			log.Fatal(err)
+		}
+	}
 }
