d3f89c8136f02bf02c059da223ecacb53098a5da
Simon Jeong <simon@myika.co>
2021-05-02 14:37:31 +0900

    working on logic for scatter

--- analytics_handlers.go
+++ analytics_handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"math/rand"
 	"net/http"
 	"time"
@@ -121,7 +122,52 @@ func scatterHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	duration := make(map[int]string)
+	collection := make(map[int][]TradeAction)
 	// var duration []int64
+	for _, x := range tradeAction {
+		if len(collection[x.AggregateID]) == 0 {
+			var taArr []TradeAction
+			taArr = append(taArr, x)
+			collection[x.AggregateID] = taArr
+		} else {
+			collection[x.AggregateID] = append(collection[x.AggregateID], x)
+		}
+	}
+
+	for key, value := range collection {
+		fmt.Println(key, value)
+		var largerNumber, temp1 time.Time
+		var lowerNumber, temp2 time.Time
+
+		for _, element := range value {
+			layout := "2006-01-02_15:04:05_-0700"
+			str := element.Timestamp
+			time, _ := time.Parse(layout, str)
+			fmt.Println(time)
+			fmt.Println(time.Sub(temp1).Minutes())
+			if time.Sub(temp1).Minutes() > 0 {
+				temp1 = time
+				largerNumber = temp1
+			}
+		}
+
+		for _, element := range value {
+			layout := "2006-01-02_15:04:05_-0700"
+			str := element.Timestamp
+			time, _ := time.Parse(layout, str)
+			// fmt.Println(time)
+			// fmt.Println(time.Sub(temp2).Minutes())
+			if time.Sub(temp2).Minutes() < 0 {
+				temp2 = time
+				lowerNumber = temp2
+			}
+		}
+		fmt.Println(" ")
+		fmt.Println(largerNumber)
+		fmt.Println(lowerNumber)
+		fmt.Println(" ")
+	}
+
 	for _, x := range tradeAction {
 		// tickerData = append(tickerData, x.AggregateID)
 		if duration[x.AggregateID] == "" {
