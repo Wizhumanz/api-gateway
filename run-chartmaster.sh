448acd20058075217e01f09b31b0dacea29f923c
Mika Yeap <yeapmika@gmail.com>
2021-05-09 11:13:34 +0800

    cloud redis

---
+++ run-chartmaster.sh
@@ -0,0 +1,11 @@
+export PORT=8000
+
+export REDISHOST=35.240.201.212
+
+export REDISPORT=6379
+
+export REDISPASS=chartm@\$terGainzSoW0W1828738701082877673617969420
+
+go build -o build/api .
+
+build/api
